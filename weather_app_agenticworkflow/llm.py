611957465c0d2b5ed6b6447328b52144ed0b06a2
Pratik Sharma <Pratiksharma@live.in>
2024-11-10 11:45:40 +0100

    llm setup

--- weather_app_agenticworkflow/llm.py
+++ weather_app_agenticworkflow/llm.py
@@ -1,10 +1,10 @@
 import os
-from langchain_openai import AzureChatOpenAI
+from crewai import LLM
 
 def getLLM():
-    return AzureChatOpenAI(
-    openai_api_version=os.environ.get("AZURE_OPENAI_VERSION", "VERSION"),
-    azure_deployment=os.environ.get("AZURE_OPENAI_DEPLOYMENT", "MODEL"),
-    azure_endpoint=os.environ.get("AZURE_OPENAI_ENDPOINT", "BASE_PATH"),
-    api_key=os.environ.get("KEY")
-)
+    return LLM(
+        model="azure/modelname",
+        api_version="version",
+        api_key="key",
+        base_url="base-url"
+    )
